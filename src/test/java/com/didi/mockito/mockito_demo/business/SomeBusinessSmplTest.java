e9d0689384ae9404c825d981dc202b79fab8d851
dihia <dihiasalhi@gmail.com>
2025-12-11 00:24:05 -0500

    feat: Simplifying tests with mockito Annotations: @Mock @InjectMocks

--- src/test/java/com/didi/mockito/mockito_demo/business/SomeBusinessSmplTest.java
+++ src/test/java/com/didi/mockito/mockito_demo/business/SomeBusinessSmplTest.java
@@ -1,26 +1,38 @@
 package com.didi.mockito.mockito_demo.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessSmplTest {
+    @Mock
+    private DataService dataService;
+
+    @InjectMocks
+    private SomeBusinessSmpl someBusinessSmpl;
 
     @Test
     void findTheGreatestFromAllData_basic() {
-        DataService dataService= mock(DataService.class);
         when(dataService.retreiveAllData()).thenReturn(new int[]{5,25,13});
-        SomeBusinessSmpl someBusinessSmpl = new SomeBusinessSmpl(dataService);
         assertEquals(25, someBusinessSmpl.findTheGreatestFromAllData());
     }
 
     @Test
     void findTheGreatestFromAllData_OneValue() {
-        DataService dataService= mock(DataService.class);
         when(dataService.retreiveAllData()).thenReturn(new int[]{13});
-        SomeBusinessSmpl someBusinessSmpl = new SomeBusinessSmpl(dataService);
         assertEquals(13, someBusinessSmpl.findTheGreatestFromAllData());
+    }
+
+    @Test
+    void findTheGreatestFromAllData_EmptyArray() {
+        when(dataService.retreiveAllData()).thenReturn(new int[]{});
+        assertEquals(Integer.MIN_VALUE, someBusinessSmpl.findTheGreatestFromAllData());
     }
 }
